4b515a82b3cab41533f0cc30f0b580dd7073fe5a
INDRANIL ROY <46955059+INDRANILROY5198@users.noreply.github.com>
2019-01-29 09:05:15 +0530

    Update about.py

--- Indu/packages/about.py
+++ Indu/packages/about.py
@@ -7,21 +7,20 @@ class thisme:
 	def __init__(self):
 		pass
 		print ""
-		print "        ::: About Nmapii :::"
+		print "        ::: About Our Software :::"
 		print ""
 	def itsme(self):
 			me = '''
-Simple NMAP automated script Coded by !NDi G3@r / Chaitanya. which saves time learning and setting nmap commands. 
+Simple NMAP automated script Coded by INDRANIL-SEKHAR-TRIDEEP which saves time learning and setting nmap commands. 
 
-lol its basically meant for ppl like me :V lazy sitting on desk for hours. 
+
 
 feel free making changes. Its an open source project :) 
 
-Special thanks to VirKid & r00t-3xp10it :) 
 
-Greets : SSA [Suspicious Shell Activity] 
 
-webs : http://hackinguyz.blogspot.com 			
+
+			
 				  '''
 			print me
 
